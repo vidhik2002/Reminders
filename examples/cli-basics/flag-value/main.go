55e464386d72089dba36ca84e661bd9a64182048
vidhik2002 <vidhik2002@gmail.com>
2021-08-29 18:33:04 +0530

    feat: cli timestamp

--- examples/cli-basics/flag-value/main.go
+++ examples/cli-basics/flag-value/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"strings"
+	"time"
+)
+
+//custom function
+type idsFlag []string
+
+func (ids idsFlag) String() string{
+	return strings.Join(ids, ",")
+}
+
+func (ids *idsFlag) Set(id string) error {
+	*ids = append(*ids, id)
+	return nil
+} 
+
+type person struct {
+	name string
+	born time.Time
+}
+
+func (p person) String() string {
+	return fmt.Sprintf("%s , %s", p.name, p.born.String())
+}
+func (p *person) Set(name string) error {
+	p.name = name
+	p.born = time.Now()
+	return nil
+}
+
 func main() {
-	
+	var ids idsFlag
+	var p person
+	flag.Var(&ids, "id", "id to be appended to list")
+	flag.Var(&p, "name", "the name of the person")
+	flag.Parse()
+	fmt.Println(ids)
+	fmt.Println(p)
 }
