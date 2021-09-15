7244378915956d18332cd40d428db8811ed6bcec
vidhik2002 <vidhik2002@gmail.com>
2021-09-16 00:29:43 +0530

    feat: helper flag

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,22 @@
 package client
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"test3/Reminders/client"
+)
+
+var (
+	backendURIFlag = flag.String("backend", "http://localhost:8080", "Backend URI")
+	helpFlag = flag.Bool("help", false, "displays the help message to direct functionality")
+)
+
+func main() {
+	flag.Parse()
+	client.NewSwitch(*backendURIFlag)
+	if *helpFlag || len(os.Args) == 1 {
+		fmt.Println("help command")
+		return
+	}
+}
